29865e7c9f69f18a86dff3938a874b612d6ccbfb
shubham gaikwad <shubhamgaikwad2k4@gmail.com>
2025-07-05 12:06:30 +0530

    add user registration

--- Backend/app.js
+++ Backend/app.js
@@ -2,13 +2,21 @@ const dotenv = require('dotenv')
 dotenv.config();
 const express = require('express');
 const cors = require('cors')
+const connectToDb = require('./db/db')
+const userRoutes = require('./routes/user.route')
 
 const app = express();
+connectToDb()
+
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Setup!')
 })
+
+app.use('/users', userRoutes)
 
 
 
